81d717632126e48ea1641ff48a086cb40b1c8e48
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-04-06 11:29:43 +0700

    done easy medium

--- hard/index.js
+++ hard/index.js
@@ -13,23 +13,109 @@ function generateBoard() {
     return board;
 }
 
-function lompatanKuda() {
+function lompatanKuda(str) {
+    str = str.split('');
     let board = generateBoard();
-    let coor = [1, 1]
+    let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+    let number = [0, 1, 2, 3, 4, 5, 6, 7];
+    let first = alphabet.indexOf(str[0]);
+    let last = parseInt(str[1]) - 1;
+    let coor = [first, last];
+    let counter = 0;
 
-    for (let i = 0; i < board.length; i++) {
+    for (let i = board.length - 1; i >= 0 ; i--) {
         for (let j = 0; j < board[i].length; j++) {
             if (i === coor[0] && j === coor[1]) {
                 board[i][j] = 'K';
+                if (i + 1 !== undefined) {
+                    if (i + 2 !== undefined) {
+                        if (i + 2) {
+                            if (j - 1 !== undefined) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+
+                if (i + 1 !== undefined) {
+                    if (i + 2 !== undefined) {
+                        if (i + 2) {
+                            if (j - 1 !== undefined) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+
+                if (i - 1 !== undefined) {
+                    if (i - 2 !== undefined) {
+                        if (i - 2) {
+                            if (j + 1 !== undefined) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+
+                if (i - 1 !== undefined) {
+                    if (i - 2 !== undefined) {
+                        if (i - 2) {
+                            if (j - 1 !== undefined) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+
+                if (j + 1 !== undefined) {
+                    if (j + 2 !== undefined) {
+                        if (j + 2) {
+                            if (i + 1 !== undefined) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+
+                if (j + 1 !== undefined) {
+                    if (j + 2 !== undefined) {
+                        if (j + 2) {
+                            if (i - 1 !== undefined) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+
+
+                if (j - 1 !== undefined) {
+                    if (j - 2 !== undefined) {
+                        if (j - 2) {
+                            if (i + 1 !== undefined) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
+
+
+                if (j - 1 !== undefined) {
+                    if (j - 2 !== undefined) {
+                        if (j - 2) {
+                            if (i - 1 !== undefined) {
+                                counter++;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 
-    console.log(board);
+    console.log(board.reverse())
+    return counter;
 }
 
-// console.log(lompatanKuda('A1')) // 2
+console.log(lompatanKuda('A1')) // 2
 // console.log(lompatanKuda('G7')) // 4
 // console.log(lompatanKuda('C4')) // 8
-
-lompatanKuda()
