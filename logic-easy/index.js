330a8920bd8a52327417eb1d6f3a5902cb958de5
Natascha Lamsu <tazcha15@gmail.com>
2018-04-06 11:29:43 +0700

    live code logic

--- logic-easy/index.js
+++ logic-easy/index.js
@@ -1,1 +1,26 @@
 //your code here
+function dominoCard(cards, cardsOnDeck) {
+  var add = []
+  for(var i = 0; i < cards.length; i++) {
+    // console.log(cards[i][0], cards[i][1]);
+    for(var j = 0; j < cards[i].length; j++) {
+      if(cardsOnDeck[0] === cards[i][j] || cardsOnDeck[1] === cards[i][j]) {
+        // console.log(cards[i]);
+        add.push(cards[i][0] + cards[i][1])
+      }
+    }
+  }
+  console.log(add);
+  var index = 0
+  for(var k = 0; k < add.length; k++) {
+    if(Math.max(...add) == add[k]) {
+      index = k
+      return 'keluarkan kartu ' + cards[index]
+    }
+  }
+  return 'tutup'
+}
+
+console.log(dominoCard([[3,3],[6,5],[3,4],[2,1]], [3,5])) //”keluarkan kartu [6,5]”
+console.log(dominoCard([[3,3],[6,5],[3,4],[2,1]], [1,3])) //"keluarkan kartu [3,4]"
+console.log(dominoCard([[2,4],[6,6],[3,6]],[1,5])) //”tutup 1 kartu”
--- medium/snake.js
+++ medium/snake.js
@@ -1,5 +1,26 @@
 function generateSnake(row, column) {
   //your code here
+  var board = []
+  for(var i = 0; i < row; i++) {
+    var arrRow = []
+    for(var j = 0; j < column; j++) {
+
+      if(i === 0 && j === 0) {
+        arrRow.push('~')
+      } else if(i === row -1 && j == column - 1) {
+        arrRow.push('o')
+      } else if(i > 0 && i % 2 !== 0 && j == column - 1) {
+        arrRow.push('|')
+      } else if(i > 0 && i % 2 === 0 && j == 0) {
+        arrRow.push('|')
+      }
+      else {
+        arrRow.push('_')
+      }
+    }
+    board.push(arrRow.join(' '))
+  }
+  console.log(board.join('\n'))
 }
 
-generateSnake(3, 6);
+generateSnake(6, 10);
